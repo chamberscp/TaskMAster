e9951dc1afa124381ca4cbf6b128c42b1f70b0fc
Chris Chambers <christopher.p.chambers@gmail.com>
2022-10-17 15:03:36 -0400

    Layout Code

---
+++ taskmaster.py
@@ -0,0 +1,34 @@
+# Import modules
+# Breakout of functions
+    # Login/ Greeting
+    # Display tasks
+    # Add Tasks
+    # Remove Tasks
+    # Show whole list
+    # Count remaining tasks in list
+    # Exit
+    
+import sys
+import datetime
+
+print(""" Greetings.  Please enter you username """)
+  
+
+def greet(name):
+   pass
+    
+
+def displaytop5(tasks):
+    pass
+   
+def addtask():
+    pass
+
+def deltask():
+    pass
+
+def counttasks():
+    pass
+
+def exitmsg():
+    pass
