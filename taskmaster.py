1cded2f26a4298f2236d669c917a5816bf92a60f
Chris Chambers <christopher.p.chambers@gmail.com>
2022-10-17 23:50:11 -0400

    Updated code and created taskmaster.sh

--- taskmaster.py
+++ taskmaster.py
@@ -1,6 +1,7 @@
 # Import modules
 # Breakout of functions
     # Login/ Greeting
+    # Help Section/ Commands
     # Display tasks
     # Add Tasks
     # Remove Tasks
@@ -8,19 +9,17 @@
     # Count remaining tasks in list
     # Exit
     
-from curses.ascii import isalpha
+
 import sys
-import datetime
 from pyfiglet import Figlet
-import sqlite3
+import datetime
 
-con = sqlite3.connect("taskmasterdb.db")
-
+#Figletdisplays the TASK MASTER 3000 Logo in "big" wordart format
 f = Figlet(font='big')
 print(f.renderText('Task Master 3000'))
 
 count = 0
-
+#Login Section.  Requires the user to login (case sensative).  User will have 3 attempts to login before being booted.
 while count < 3:
     count = 0
     user_list = ["Elliot", "Chris"]
@@ -31,19 +30,19 @@ while count < 3:
     else:
         print("Unauthorized user.  Please enter valid username: ")                            
                     
+# Commands Section - Defines the commands the user can input.
 
-def addtask():
-    pass
-
+def commands():
+    avcmds = """Commands 
+    ./task add "task name"      - Adds a new task
+    ./task delete NUMBER        - Deletes a task
+    ./task show                 - Shows all remaining tasks and provides a count of remaining tasks
+    ./task commands             - Shows this Commands Section"""
+    sys.stdout.buffer.write(avcmds.encode('utf8'))
+    
+# Quick display of the top five tasks in the order they were input (via timestamp)
 def displaytop5(tasks):
-    pass
-   
+    pass   
 
-def deltask():
-    pass
 
-def counttasks():
-    pass
-
-def exitmsg():
-    pass
+ 
---
+++ taskmaster.sh
@@ -0,0 +1,1 @@
+python taskmaster.py %1 %2
