979ee3f0dc6fb0d5dfe292ab1349318b1dda60aa
Chris Chambers <christopher.p.chambers@gmail.com>
2022-10-26 15:39:05 -0400

    Add complete function

--- taskmaster.py
+++ taskmaster.py
@@ -2,6 +2,8 @@
 
 import sys
 import  os
+import datetime
+import time
 from pyfiglet import Figlet
 
 #Figletdisplays the taskmaster MASTER 3000 Logo in "big" wordart format
@@ -12,8 +14,9 @@ print(f.renderText('Task Master 3000'))
 x = """              Task Master 3000 Commands:
 ./taskmaster add "task name"  		# Adds new tasks
 ./taskmaster show               	# Show all remaining tasks
-./taskmaster delete "task number"       # Delete a task
-./taskmaster count                      # Counts the number of remaining tasks
+./taskmaster complete NUMBER        # Marks a task as complete. If it is the first task, it will be deleted.
+./taskmaster delete NUMBER          # Delete a task
+./taskmaster count                  # Counts the number of remaining tasks
 """
 print(x)
 
@@ -29,6 +32,7 @@ else:
                     
 # Function to add tasks
 def add(newtask):
+    #add enumerate here????
     f = open(user_name +'.txt', 'a')
     f.write(newtask)
     f.write("\n")
@@ -37,11 +41,35 @@ def add(newtask):
     print(f"Added the task: {newtask}") 
     show()
    
+ # Function to Complete a todo
+def complete(num):
+    try:
+        update()
+        num = int(num)
+        f = open(user_name + '.txt', 'a')
+        st = 'x '+str(datetime.datetime.today()).split()[0]+' '+d[num]
+        f.write(st)
+        f.write("\n")
+        f.close()
+        print(f"Marked task #{num} as complete. Getting your new list. Please wait....")
+        time.sleep(3)
+        with open(user_name +'.txt', 'r+') as f:
+            lines = f.readlines()
+            f.seek(0)
+			
+            for i in lines:
+                if i.strip('\n') != d[num]:
+                    f.write(i)
+            f.truncate()
+            show()
+    except:
+        print(f"There is no task #{num}.")
+  
 # Function that deletes a task
 def delete(number):
     try:
         number = int(number)
-        ud()
+        update()
         with open(user_name +'.txt', "r+") as f:
             lines = f.readlines()
             f.seek(0)
@@ -50,10 +78,8 @@ def delete(number):
                     f.write(i)
             f.truncate()
         print(f"Deleted task #{number}")
-        
- 
     except Exception as e:
-        print(f" There is no task #{number}.")
+        print(f"There is no task #{number}.")
         show()
         
  # Function that shows a list of tasks
@@ -62,7 +88,7 @@ def show():
         if os.path.getsize(user_name +'.txt') <1:
             print("You don't have any tasks")
         else:    
-        #    for i, line in enumerate(q, start=1):
+        #   for i, line in enumerate(q, start=1):
         #        pass
             data = q.read().splitlines()
             total_lines=len(data)
@@ -82,7 +108,7 @@ def show():
         
                 
 # Function to update list of tasks
-def ud():
+def update():
     try:
         f = open(user_name +'.txt', 'r')
         c = 1
@@ -107,16 +133,23 @@ if __name__ == '__main__':
         d = {}
         don = {}
         args = sys.argv
+        
         if(args[1] == 'delete'):
             args[1] = 'delete'
+        
         if(args[1] == 'add' and len(args[2:]) == 0):
             print("You forgot to list the task after the add command")
+            
+        elif(args[1] == 'done' and len(args[2:]) ==0):
+            print("You forgot to tell me what number to mark as complete.  Please try again")
+        
         elif(args[1] == 'delete' and len(args[2:]) == 0):
             print("You forgot to tell me what number to delete.  Please try again")
+        
         else:
             globals()[args[1]](*args[2:])
     finally:
-            ud()
+            update()
         
         
         
